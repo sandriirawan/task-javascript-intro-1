b9d6dda0806bc7075482d776b5f8d485046231ea
sandriirawan <80002249+sandriirawan@users.noreply.github.com>
2023-06-02 15:10:37 +0700

    first commit

--- script.js
+++ script.js
@@ -1,80 +1,87 @@
-// const biodata = {
-//     name: "sandri irawan",
-//     age: 21,
-//     hobbies:["Traveling", "Musik", "game"],
-//     isMaried: false,
-//     schoolList: [
-//         {
-//             name: "SD INDUSTRI",
-//             yearIn: 2007,
-//             tahunOut: 2012,
-//             major:null,
-//         },
-//         { 
-//             name: "SMP LPPN",
-//             yearIn: 2013,
-//             tahunOut: 2016,
-//             major:null,
-//         },
-//         {
-//             name: "SMA LPPN",
-//             yearIn: 2016,
-//             tahunOut: 20119,
-//             major: "IPA",
-//         },
-//     ],
-//     skill: [
-//         {
-//             skillName:"coding",
-//             level:"beginer",
-//         },
-//         {
-//             skillName:"game",
-//             level:"expert",
-//         }
-//     ],
-//     interestinCoding: true,
+// no1
 
-// }
+const biodata = {
+    name: "sandri irawan",
+    age: 21,
+    hobbies:["Traveling", "Musik", "game"],
+    isMaried: false,
+    schoolList: [
+        {
+            name: "SD INDUSTRI",
+            yearIn: 2007,
+            tahunOut: 2012,
+            major:null,
+        },
+        { 
+            name: "SMP LPPN",
+            yearIn: 2013,
+            tahunOut: 2016,
+            major:null,
+        },
+        {
+            name: "SMA LPPN",
+            yearIn: 2016,
+            tahunOut: 20119,
+            major: "IPA",
+        },
+    ],
+    skill: [
+        {
+            skillName:"coding",
+            level:"beginer",
+        },
+        {
+            skillName:"game",
+            level:"expert",
+        }
+    ],
+    interestinCoding: true,
 
-// console.log(biodata);
+}
+
+console.log(biodata);
 
 
 // ---------------------------------------------------------------
-// const mtk = 80
-// const bahasaIndonesia = 90
-// const bahasaInggris = 89
-// const ipa = 69
-// const result = (mtk + bahasaIndonesia + bahasaInggris + ipa) / 4
+// no2
+const mtk = 80
+const bahasaIndonesia = 90
+const bahasaInggris = 89
+const ipa = 69
+const result = (mtk + bahasaIndonesia + bahasaInggris + ipa) / 4
 
-// function rata2(){
-//     if(result >= 90){
-//         console.log(result + ' grade a') 
-//     }else if(result >= 79 && result <= 89){
-//         console.log(result +' grade b') 
-//     }
-//     else if(result >= 70 && result <= 79){
-//         console.log(result + ' grade c') 
-//     }
-//     else if(result >= 60 && result <= 69){
-//         console.log(result + ' grade d') 
-//     }
-//     else if(result <= 59){``
-//         console.log(result + ' grade e') 
-//     } 
-// }
-// rata2();
+function rata2(){
+    if(result >= 90){
+        console.log(result + ' grade a') 
+    }else if(result >= 79 && result <= 89){
+        console.log(result +' grade b') 
+    }
+    else if(result >= 70 && result <= 79){
+        console.log(result + ' grade c') 
+    }
+    else if(result >= 60 && result <= 69){
+        console.log(result + ' grade d') 
+    }
+    else if(result <= 59){``
+        console.log(result + ' grade e') 
+    } 
+}
+rata2();
 
-// const printSegitiga = 5
 
-// for(let i = printSegitiga; i >= 1; i--){
-//     let output = " " ;
-//     for (let x = 1; x <= i; x++){
-//         output += x + " ";
-//     }
-//     console.log(output);
-// }
+// ---------------------------------------------------------------
+// no3
+const printSegitiga = 5
+for(let i = printSegitiga; i >= 1; i--){
+    let output = " " ;
+    for (let x = 1; x <= i; x++){
+        output += x + " ";
+    }
+    console.log(output);
+}
 
+// ---------------------------------------------------------------
+// no4
 let data = {
     id: 1,
     name: "Leanne Graham",
