85991b239ebf36f19da204af376cd7d4171c4f07
sandriirawan <80002249+sandriirawan@users.noreply.github.com>
2023-06-05 12:10:09 +0700

    merambahkan fungsi

--- soal3.js
+++ soal3.js
@@ -1,11 +1,17 @@
-const printSegitiga = 5
-if(typeof printSegitiga !== "number"){
-    console.log("data harus angka");
-}
-for(let i = printSegitiga; i >= 1; i--){
-    let output = " " ;
-    for (let x = 1; x <= i; x++){
-        output += x + " ";
+function printTriangle(printSegitiga) {
+  if (typeof printSegitiga !== "number") {
+    console.log("Data harus angka");
+    return;
+  }
+
+  for (let i = printSegitiga; i >= 1; i--) {
+    let output = " ";
+    for (let x = 1; x <= i; x++) {
+      output += x + " ";
     }
     console.log(output);
+  }
 }
+
+const printSegitiga = 5;
+printTriangle(printSegitiga);
