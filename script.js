e7be51e1ddeaa22cb98b6dc7ba59d1230605085b
sandriirawan <80002249+sandriirawan@users.noreply.github.com>
2023-06-05 10:30:16 +0700

    menambahkan validasi

--- script.js
+++ script.js
@@ -1,54 +1,58 @@
 // no1
 
-const biodata = {
-    name: "sandri irawan",
-    age: 21,
-    hobbies:["Traveling", "Musik", "game"],
-    isMaried: false,
-    schoolList: [
-        {
-            name: "SD INDUSTRI",
-            yearIn: 2007,
-            tahunOut: 2012,
-            major:null,
-        },
-        { 
-            name: "SMP LPPN",
-            yearIn: 2013,
-            tahunOut: 2016,
-            major:null,
-        },
-        {
-            name: "SMA LPPN",
-            yearIn: 2016,
-            tahunOut: 20119,
-            major: "IPA",
-        },
-    ],
-    skill: [
-        {
-            skillName:"coding",
-            level:"beginer",
-        },
-        {
-            skillName:"game",
-            level:"expert",
-        }
-    ],
-    interestinCoding: true,
+// const biodata = {
+//     name: "sandri irawan",
+//     age: 21,
+//     hobbies:["Traveling", "Musik", "game"],
+//     isMaried: false,
+//     schoolList: [
+//         {
+//             name: "SD INDUSTRI",
+//             yearIn: 2007,
+//             tahunOut: 2012,
+//             major:null,
+//         },
+//         { 
+//             name: "SMP LPPN",
+//             yearIn: 2013,
+//             tahunOut: 2016,
+//             major:null,
+//         },
+//         {
+//             name: "SMA LPPN",
+//             yearIn: 2016,
+//             tahunOut: 20119,
+//             major: "IPA",
+//         },
+//     ],
+//     skill: [
+//         {
+//             skillName:"coding",
+//             level:"beginer",
+//         },
+//         {
+//             skillName:"game",
+//             level:"expert",
+//         }
+//     ],
+//     interestinCoding: true,
 
-}
+// }
 
-console.log(biodata);
+// console.log(biodata);
 
 
 // ---------------------------------------------------------------
+
 // no2
 const mtk = 80
 const bahasaIndonesia = 90
 const bahasaInggris = 89
 const ipa = 69
 const result = (mtk + bahasaIndonesia + bahasaInggris + ipa) / 4
+    let cekNum = result ? "true" : "false" 
+        console.log(cekNum)
+
 
 function rata2(){
     if(result >= 90){
@@ -70,43 +74,45 @@ rata2();
 
 
 // ---------------------------------------------------------------
+
 // no3
-const printSegitiga = 5
-for(let i = printSegitiga; i >= 1; i--){
-    let output = " " ;
-    for (let x = 1; x <= i; x++){
-        output += x + " ";
-    }
-    console.log(output);
-}
+// const printSegitiga = 5
+// for(let i = printSegitiga; i >= 1; i--){
+//     let output = " " ;
+//     for (let x = 1; x <= i; x++){
+//         output += x + " ";
+//     }
+//     console.log(output);
+// }
 
-// ---------------------------------------------------------------
+// // ---------------------------------------------------------------
+
 // no4
-let data = {
-    id: 1,
-    name: "Leanne Graham",
-    username: "Bret",
-    email: "Sincere@april.biz",
-    address: 
-    {
-    street: "Kulas Light",
-    suite: "Apt. 556",
-    city: "Gwenborough",
-    zipcode: "92998-3874",
-    },
-    phone: "1-770-736-8031 x56442",
-    website: "hildegard.org",
-    }
+// let data = {
+//     id: 1,
+//     name: "Leanne Graham",
+//     username: "Bret",
+//     email: "Sincere@april.biz",
+//     address: 
+//     {
+//     street: "Kulas Light",
+//     suite: "Apt. 556",
+//     city: "Gwenborough",
+//     zipcode: "92998-3874",
+//     },
+//     phone: "1-770-736-8031 x56442",
+//     website: "hildegard.org",
+//     }
     
-    let newData = {
-        ...data,
-        name: "sandri irawan",
-        email: "sandriirawan01@gmail.com",
-        hobby: "game"
-      };
-      console.log(newData);
+    // let newData = {
+    //     ...data,
+    //     name: "sandri irawan",
+    //     email: "sandriirawan01@gmail.com",
+    //     hobby: "game"
+    //   };
+    //   console.log(newData);
 
-      const {address:{street, city}} = data;
-      console.log(street);
-      console.log(city);
+    //   const {address:{street, city}} = data;
+    //   console.log(street);
+    //   console.log(city);
     
